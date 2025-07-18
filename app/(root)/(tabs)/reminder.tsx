bacbffe1f388ca1214e97f84df20132e0dab0436
Amit Ramrakhyani <amit.ramrakhyani1109@gmail.com>
2025-07-18 20:03:30 +0530

    feat: add date wise reminders sections (past, upcoming, no alert, etc...), format dates, clean UI

--- app/(root)/(tabs)/reminder.tsx
+++ app/(root)/(tabs)/reminder.tsx
@@ -2,36 +2,39 @@ import ReminderItem from "@/components/ReminderItem";
 import ReminderToolBar from "@/components/ReminderToolBar";
 import { ReminderFieldProps } from "@/types/type";
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
-const reminders: ReminderFieldProps[] = [
+// Set const time to 18 July, 2025 16:20 PM
+const date = new Date(2025, 6, 18, 16, 17);
+
+export const reminders: ReminderFieldProps[] = [
   {
     id: "1",
     title: "Reminder 1",
     description: "Description 1",
-    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+    dueDateTime: date,
     createdAt: new Date(),
+    status: "pending",
+    priority: "high",
   },
   {
     id: "2",
     title: "Reminder 2",
     description: "Description 2",
-    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 2),
+    dueDateTime: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 2),
     createdAt: new Date(),
-  },
-  {
-    id: "3",
-    title: "Reminder 3",
-    description: "Description 3",
-    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 3),
-    createdAt: new Date(),
+    status: "pending",
+    priority: "medium",
   },
   {
     id: "4",
     title: "Reminder 4",
     description: "Description 4",
-    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 4),
+    // dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 4),
     createdAt: new Date(),
+    status: "completed",
+    completedAt: new Date(),
+    priority: "low",
   },
   {
     id: "5",
@@ -39,6 +42,8 @@ const reminders: ReminderFieldProps[] = [
     description: "Description 5",
     dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 5),
     createdAt: new Date(),
+    status: "pending",
+    priority: "medium",
   },
   {
     id: "6",
@@ -46,29 +51,125 @@ const reminders: ReminderFieldProps[] = [
     description: "Description 6",
     dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 6),
     createdAt: new Date(),
+    status: "pending",
+    priority: "low",
+  },
+  {
+    id: "3",
+    title: "Reminder 3",
+    description: "Description 3",
+    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 1),
+    createdAt: new Date(),
+    status: "pending",
+  },
+  {
+    id: "7",
+    title: "Reminder 7",
+    description: "Description 7",
+    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7),
+    createdAt: new Date(),
+  },
+  {
+    id: "8",
+    title: "Reminder 8",
+    description: "Description 8",
+    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 8),
+    createdAt: new Date(),
+  },
+  {
+    id: "9",
+    title: "Reminder 9",
+    description: "Description 9",
+    dueDateTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 9),
+    createdAt: new Date(),
   },
 ];
 
 const Reminder = () => {
+  const today = new Date();
+  const endOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    23,
+    59,
+    59
+  );
+
+  const startOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    0,
+    0,
+    0
+  );
+
+  const sortedReminders = reminders.sort(
+    (a, b) => (a.dueDateTime?.getTime() || 0) - (b.dueDateTime?.getTime() || 0)
+  );
+
+  const pastReminders = sortedReminders.filter(
+    (reminder) => reminder.dueDateTime && reminder.dueDateTime < new Date()
+  );
+
+  const todayReminders = sortedReminders.filter(
+    (reminder) =>
+      reminder.dueDateTime &&
+      reminder.dueDateTime > new Date() &&
+      reminder.dueDateTime <= endOfDay
+  );
+
+  const soonReminders = sortedReminders.filter(
+    (reminder) => reminder.dueDateTime && reminder.dueDateTime > endOfDay
+  );
+
+  const noAlarmReminders = sortedReminders.filter(
+    (reminder) => !reminder.dueDateTime
+  );
+
   return (
     <View className="flex-1 bg-black">
-      <View className="flex-1 bg-white mt-10">
+      <View className="flex-1 bg-gray-50 mt-10">
         <ReminderToolBar />
-        <View className="flex-1 bg-white mx-2">
-          <FlatList
-            data={reminders}
-            renderItem={({ item }) => <ReminderItem reminder={item} />}
-            keyExtractor={(item) => item.id}
-            contentContainerStyle={{ paddingBottom: 100 }}
-            ListHeaderComponent={() => (
-              <View className="flex-1 items-center justify-center bg-white my-10">
-                <Text className="text-2xl font-bold">Reminders</Text>
-              </View>
-            )}
-          />
-        </View>
+        <ScrollView className="flex-1 bg-gray-50 mx-2 pt-3">
+          {pastReminders.length > 0 && (
+            <ReminderSection title="Past" reminders={pastReminders} />
+          )}
+          {todayReminders.length > 0 && (
+            <ReminderSection title="Today" reminders={todayReminders} />
+          )}
+          {soonReminders.length > 0 && (
+            <ReminderSection title="Upcoming" reminders={soonReminders} />
+          )}
+          {noAlarmReminders.length > 0 && (
+            <ReminderSection title="No Alert" reminders={noAlarmReminders} />
+          )}
+
+          {/* Buttom padding */}
+          <View className="h-20 mb-14" />
+        </ScrollView>
       </View>
     </View>
+  );
+};
+
+const ReminderSection = ({
+  title,
+  reminders,
+}: {
+  title: string;
+  reminders: ReminderFieldProps[];
+}) => {
+  return (
+    <>
+      <View className="flex-1 items-start justify-center mx-4 mt-4">
+        <Text className="text-xl font-HelveticaNeueMedium">{title}</Text>
+      </View>
+      {reminders.map((reminder) => (
+        <ReminderItem key={reminder.id} reminder={reminder} />
+      ))}
+    </>
   );
 };
 
--- components/ReminderItem.tsx
+++ components/ReminderItem.tsx
@@ -1,19 +1,58 @@
 import { ReminderFieldProps } from "@/types/type";
+import { router } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
+
+// Format - Yesterday, 11:00 | Today, 16:00 | Tomorrow, 12:00 | Wed, 24 Jul, 15:00
+const formatDate = ({ date }: { date: Date }) => {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const isYesterday = date.toDateString() === yesterday.toDateString();
+  const isToday = date.toDateString() === new Date().toDateString();
+  const isTomorrow = date.toDateString() === tomorrow.toDateString();
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  let dateString = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+  }).format(date);
+
+  if (isYesterday) dateString = "Yesterday";
+  if (isToday) dateString = "Today";
+  if (isTomorrow) dateString = "Tomorrow";
+
+  return `${dateString}, ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+};
 
 const ReminderItem = ({ reminder }: { reminder: ReminderFieldProps }) => {
   return (
-    <View className="bg-white rounded-3xl shadow-[0px_0px_2px_0px_#1a202c] m-2 p-2 mb-6">
-      <TouchableOpacity className="flex-row items-center py-4 px-2 border-b border-gray-100 last:border-b-0">
+    <View className="bg-white rounded-3xl m-2 p-2">
+      <TouchableOpacity
+        onPress={() => router.push(`/reminder/${reminder.id}`)}
+        className="flex-row items-center py-2 px-2 border-b border-gray-100 last:border-b-0"
+      >
         <View className="flex-row items-start">
-          <TouchableOpacity className="w-7 h-7 rounded-full border border-gray-300 mr-4" />
-          <View className="flex-col">
-            <Text className="text-lg text-black font-HelveticaNeueMedium mb-4">
+          <TouchableOpacity className="w-6 h-6 rounded-full border-2 border-[#3A04FF] mr-4" />
+          <View className="flex-col items-start justify-between">
+            <Text className="text-lg text-black font-HelveticaNeueRomanr">
               {reminder.title}
             </Text>
-            <Text className="text-sm text-gray-500">
-              {reminder.dueDateTime?.toLocaleDateString()}
-            </Text>
+            {reminder.dueDateTime && (
+              <Text
+                className={`text-sm ${
+                  reminder.dueDateTime < new Date()
+                    ? "text-red-500"
+                    : "text-gray-500"
+                } mt-3`}
+              >
+                {formatDate({ date: reminder.dueDateTime })}
+              </Text>
+            )}
           </View>
         </View>
       </TouchableOpacity>
